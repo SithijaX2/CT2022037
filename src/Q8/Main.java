e6af29ddda19b5f87749e924b5d937cf81f38e80
wolfyblue078 <wolfyblue078@gmail.com>
2026-04-14 20:42:57 +0530

    Qustion 8 gui completed!

---
+++ src/Q8/Main.java
@@ -0,0 +1,33 @@
+package Q8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter your Text: ");
+        String text = myScanner.nextLine();
+
+        String text_1 ;
+        String text_2 ;
+
+        int text_length = text.length();
+        int text_1_Length=-1;
+
+        for (int i = 0; i < text.length(); i++) {
+            char nowChar = text.charAt(i);
+            if(nowChar == '!'){
+                text_1_Length = i+1;
+                break;
+            }
+        }
+
+        text_1 = text.substring(0,text_1_Length);
+        text_2 = text.substring(text_1_Length, text_length);
+
+
+        System.out.println(text_1 + "\n" + text_2);
+
+    }
+}
